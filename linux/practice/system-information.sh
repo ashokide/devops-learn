646bd486ede4d523af5d687fefc5bdf7747d14c8
Ashok Natarajan <66304695+ashokide@users.noreply.github.com>
2024-08-24 22:11:28 +0530

    Updated system-information.sh

--- linux/practice/system-information.sh
+++ linux/practice/system-information.sh
@@ -3,6 +3,8 @@
 #System Information
 echo "System Information"
 echo "--------------------"
+printf "Username            : %s\n" "$(whoami)"
+printf "Hostname            : %s\n" "$(hostname)"
 printf "Operating System    : %s\n" $(uname -o) 
 printf "Architecture        : %s\n" $(uname -m)
 printf "Kernel Version      : %s\n" $(uname -r) 
