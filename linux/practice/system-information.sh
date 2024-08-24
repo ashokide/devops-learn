4dbcab9477cb9d7984276d51fd5c6d93ff4dc83f
Ashok Natarajan <66304695+ashokide@users.noreply.github.com>
2024-08-24 22:27:53 +0530

    Added disk usage

--- linux/practice/system-information.sh
+++ linux/practice/system-information.sh
@@ -9,3 +9,4 @@ printf "Operating System    : %s\n" $(uname -o)
 printf "Architecture        : %s\n" $(uname -m)
 printf "Kernel Version      : %s\n" $(uname -r) 
 printf "Uptime              : %s\n" "$(uptime -p)"
+printf "Disk                : %s\n" "$(df / -h | awk 'NR > 1 { print "Used: "$4 " Total: " $2 }')"
