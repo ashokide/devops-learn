2eb71dd1e05eee875da3c1f0d52ec78e2af1561a
Ashok Natarajan <66304695+ashokide@users.noreply.github.com>
2024-09-14 23:57:35 +0530

    Added check for ntpdate

--- linux/practice/retrieve-ntp-time.sh
+++ linux/practice/retrieve-ntp-time.sh
@@ -3,6 +3,12 @@
 # NTP server to get time
 NTP_SERVER="time.google.com"
 
+# Check if ntpdate is present
+if ! command -v ntpdate &> /dev/null; then
+  echo "Error: ntpdate is not installed or not in the system's PATH."
+  exit 1
+fi
+
 # Get current time from NTP server
 ntp_time=$(ntpdate -q -s $NTP_SERVER)
 
