c2e310c5dbd4b3be93b9430ac1653d7193d5bf8a
Ashok Natarajan <66304695+ashokide@users.noreply.github.com>
2024-09-15 23:42:05 +0530

    Added function with arguments

--- linux/practice/function.sh
+++ linux/practice/function.sh
@@ -12,3 +12,19 @@ check_sudo_access() {
 }
 
 check_sudo_access
+
+# function with and without argument
+hello_world() {
+  # Check if an argument is provided
+  if [ $# -eq 0 ]; then
+    echo "Hello, World!"
+  else
+    echo "Hello, $1!"
+  fi
+}
+
+# Call the function with an argument
+hello_world "$USER"
+
+# Call the function without an argument
+hello_world
